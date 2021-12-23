e34d327c2711a52f2c99a73dc293d5bf2dd0bae3
Reuben Ramos <rsramos23@gmail.com>
2021-12-22 19:58:06 -0800

    final project

---
+++ build/precache-manifest.116ea4f81a38d1d5985825a1fba03bb4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee4484366435463c57520a9f1960a62c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8adb3a1cab9e6012fcc7",
+    "url": "/static/js/2.6c99d600.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.6c99d600.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9840a58acadd7a27dafb",
+    "url": "/static/js/main.766f4dcd.chunk.js"
+  },
+  {
+    "revision": "aa7c612222590812d412",
+    "url": "/static/js/runtime-main.1160daa0.js"
+  }
+]);
--- src/api/index.js
+++ src/api/index.js
@@ -4,7 +4,7 @@
  * You need to replace YOUR_API_KEY in the string associated with KEY with your actual API key
  */
 export const BASE_URL = 'https://api.harvardartmuseums.org';
-export const KEY = 'apikey=YOUR_API_KEY';
+export const KEY = 'apikey=b0a3fe9e-b791-4f7d-81f0-62ad53cf8386';
 
 /**
  * This will make a call to the API for a single term and value (e.g. "person", and "unknown"), and return the result
